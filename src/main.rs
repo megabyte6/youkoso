ba8493c49d47a5b09716c67686131988a57b72ee
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-05-08 13:21:04 -0700

    fix: Update theme handling in AppWindow and improve imports in Slint UI

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use reqwest::Client;
 
 slint::include_modules!();
 
+use slint_generatedAppWindow as ui;
+
 #[tokio::main]
 async fn main() -> Result<(), slint::PlatformError> {
     let config = Rc::new(RefCell::new(
@@ -22,7 +24,7 @@ async fn main() -> Result<(), slint::PlatformError> {
 
     let client = Client::new();
 
-    let app_window = Rc::new(AppWindow::new()?);
+    let app_window = AppWindow::new()?;
     app_window.impl_update_settings(&config);
     app_window.load_theme_from_config(&config.borrow());
     app_window.run()?;
@@ -33,28 +35,23 @@ async fn main() -> Result<(), slint::PlatformError> {
 impl AppWindow {
     fn load_theme_from_config(&self, config: &Config) {
         self.set_theme(match config.theme {
-            config::Theme::System => slint_generatedAppWindow::Theme::System,
-            config::Theme::Dark => slint_generatedAppWindow::Theme::Dark,
-            config::Theme::Light => slint_generatedAppWindow::Theme::Light,
+            config::Theme::System => ui::Theme::System,
+            config::Theme::Dark => ui::Theme::Dark,
+            config::Theme::Light => ui::Theme::Light,
         });
         self.invoke_reload_theme();
     }
-}
 
-trait AppWindowCallback {
-    fn impl_update_settings(&self, config: &Rc<RefCell<Config>>);
-}
-
-impl AppWindowCallback for Rc<AppWindow> {
     fn impl_update_settings(&self, config: &Rc<RefCell<Config>>) {
-        let app_window = Rc::clone(self);
-        let config = Rc::clone(config);
-
-        self.on_update_settings(move || {
-            config.borrow_mut().theme = match app_window.get_theme() {
-                Theme::System => config::Theme::System,
-                Theme::Dark => config::Theme::Dark,
-                Theme::Light => config::Theme::Light,
+        self.on_update_settings({
+            let app_window = self.as_weak();
+            let config = Rc::clone(config);
+            move || {
+                config.borrow_mut().theme = match app_window.unwrap().get_theme() {
+                    Theme::System => config::Theme::System,
+                    Theme::Dark => config::Theme::Dark,
+                    Theme::Light => config::Theme::Light,
+                }
             }
         });
     }

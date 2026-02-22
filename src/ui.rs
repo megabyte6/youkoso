bd8b6666f8ccef8cae22de914ed9792b1fb99988
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2026-02-22 00:42:35 -0800

    Remove the custom `Theme` struct, replacing it with the built-in `ColorScheme`

--- src/ui.rs
+++ src/ui.rs
@@ -1,9 +1,10 @@
 use std::{cell::RefCell, rc::Rc, time::Duration};
 
-use slint::{CloseRequestResponse, SharedString, Timer, ToSharedString, Weak};
-use slint_generatedApp as SlintGenerated;
+use slint::{
+    CloseRequestResponse, SharedString, Timer, ToSharedString, Weak, language::ColorScheme,
+};
 
-use crate::config::{self, Config};
+use crate::config::{Config, Theme};
 
 slint::include_modules!();
 
@@ -20,11 +21,12 @@ pub fn init(config: &Rc<RefCell<Config>>) -> App {
         move || {
             let strong_ui = ui.upgrade().unwrap();
             let settings = strong_ui.global::<Settings>();
+            let mut config = config.try_borrow_mut().unwrap();
             if settings.get_syncing() {
-                save_to_config(&settings, &mut config.try_borrow_mut().unwrap());
+                save_to_config(&strong_ui, &mut config);
                 settings.set_syncing(false);
             }
-            config.try_borrow().unwrap().save().unwrap();
+            config.save().unwrap();
 
             CloseRequestResponse::HideWindow
         }
@@ -56,13 +58,12 @@ fn impl_settings_page_callbacks(ui: &App, config: &Rc<RefCell<Config>>) {
             // prevent each keystroke causing full saves by only saving every 5 seconds if there
             // was a change
             Timer::single_shot(Duration::from_secs(5), move || {
-                let mut config = config.try_borrow_mut().unwrap();
                 // a strong reference to the ui
                 let strong_ui = ui.upgrade().unwrap();
-                let settings = strong_ui.global::<Settings>();
-                save_to_config(&settings, &mut config);
+                let mut config = config.try_borrow_mut().unwrap();
+                save_to_config(&strong_ui, &mut config);
 
-                settings.set_syncing(false);
+                strong_ui.global::<Settings>().set_syncing(false);
             });
         }
     });
@@ -77,63 +78,70 @@ fn impl_settings_page_callbacks(ui: &App, config: &Rc<RefCell<Config>>) {
             let settings = strong_ui.global::<Settings>();
             match id.as_str() {
                 "theme" => {
-                    config.theme = Config::default().theme.clone();
-                    settings.set_theme(match config.theme {
-                        config::Theme::System => SlintGenerated::Theme::System,
-                        config::Theme::Dark => SlintGenerated::Theme::Dark,
-                        config::Theme::Light => SlintGenerated::Theme::Light,
-                    });
-                    strong_ui.invoke_reload_theme();
+                    config.theme = Config::default().theme;
+                    strong_ui
+                        .global::<Palette>()
+                        .set_color_scheme(match config.theme {
+                            Theme::System => ColorScheme::Unknown,
+                            Theme::Dark => ColorScheme::Dark,
+                            Theme::Light => ColorScheme::Light,
+                        });
                 }
                 "my-studio.email" => {
+                    config.my_studio.email = Config::default().my_studio.email;
                     let mut updated = settings.get_my_studio();
-                    updated.email = Config::default().my_studio.email.into();
+                    updated.email = config.my_studio.email.clone().into();
                     settings.set_my_studio(updated);
                 }
                 "my-studio.company-id" => {
+                    config.my_studio.company_id = Config::default().my_studio.company_id;
                     let mut updated = settings.get_my_studio();
-                    updated.company_id = Config::default().my_studio.company_id.into();
+                    updated.company_id = config.my_studio.company_id.clone().into();
                     settings.set_my_studio(updated);
                 }
                 "student-data.filepath" => {
+                    config.student_data.filepath = Config::default().student_data.filepath;
                     let mut updated = settings.get_student_data();
-                    updated.filepath = Config::default()
-                        .student_data
-                        .filepath
-                        .display()
-                        .to_shared_string();
+                    updated.filepath = config.student_data.filepath.display().to_shared_string();
                     settings.set_student_data(updated);
                 }
                 "student-data.sheet-name" => {
+                    config.student_data.sheet_name = Config::default().student_data.sheet_name;
                     let mut updated = settings.get_student_data();
-                    updated.sheet_name = Config::default().student_data.sheet_name.into();
+                    updated.sheet_name = config.student_data.sheet_name.clone().into();
                     settings.set_student_data(updated);
                 }
                 "student-data.name-column" => {
+                    config.student_data.name_column = Config::default().student_data.name_column;
                     let mut updated = settings.get_student_data();
-                    updated.name_column = Config::default().student_data.name_column.into();
+                    updated.name_column = config.student_data.name_column.into();
                     settings.set_student_data(updated);
                 }
                 "student-data.id-column" => {
+                    config.student_data.id_column = Config::default().student_data.id_column;
                     let mut updated = settings.get_student_data();
-                    updated.id_column = Config::default().student_data.id_column.into();
+                    updated.id_column = config.student_data.id_column.into();
                     settings.set_student_data(updated);
                 }
                 "student-data.immediate-sign-in-column" => {
+                    config.student_data.immediate_sign_in.column =
+                        Config::default().student_data.immediate_sign_in.column;
                     let mut updated = settings.get_student_data();
-                    updated.immediate_sign_in_column = Config::default()
-                        .student_data
-                        .immediate_sign_in
-                        .column
-                        .into();
+                    updated.immediate_sign_in_column =
+                        config.student_data.immediate_sign_in.column.into();
                     settings.set_student_data(updated);
                 }
                 "student-data.immediate-sign-in-enabled-symbol" => {
+                    config.student_data.immediate_sign_in.enabled_symbol = Config::default()
+                        .student_data
+                        .immediate_sign_in
+                        .enabled_symbol;
                     let mut updated = settings.get_student_data();
-                    updated.immediate_sign_in_enabled_symbol = Config::default()
+                    updated.immediate_sign_in_enabled_symbol = config
                         .student_data
                         .immediate_sign_in
                         .enabled_symbol
+                        .clone()
                         .into();
                     settings.set_student_data(updated);
                 }
@@ -163,14 +171,13 @@ fn impl_settings_page_callbacks(ui: &App, config: &Rc<RefCell<Config>>) {
 }
 
 fn load_config(ui: &App, config: &Config) {
-    let settings = ui.global::<Settings>();
-
-    settings.set_theme(match config.theme {
-        config::Theme::System => SlintGenerated::Theme::System,
-        config::Theme::Dark => SlintGenerated::Theme::Dark,
-        config::Theme::Light => SlintGenerated::Theme::Light,
+    ui.global::<Palette>().set_color_scheme(match config.theme {
+        Theme::System => ColorScheme::Unknown,
+        Theme::Dark => ColorScheme::Dark,
+        Theme::Light => ColorScheme::Light,
     });
-    ui.invoke_reload_theme();
+
+    let settings = ui.global::<Settings>();
 
     settings.set_my_studio(MyStudio {
         email: config.my_studio.email.clone().into(),
@@ -192,12 +199,15 @@ fn load_config(ui: &App, config: &Config) {
     });
 }
 
-fn save_to_config(settings: &Settings, config: &mut Config) {
-    config.theme = match settings.get_theme() {
-        Theme::System => config::Theme::System,
-        Theme::Dark => config::Theme::Dark,
-        Theme::Light => config::Theme::Light,
+fn save_to_config(ui: &App, config: &mut Config) {
+    config.theme = match ui.global::<Palette>().get_color_scheme() {
+        ColorScheme::Unknown => Theme::System,
+        ColorScheme::Dark => Theme::Dark,
+        ColorScheme::Light => Theme::Light,
+        _ => unreachable!(),
     };
+
+    let settings = ui.global::<Settings>();
 
     config.my_studio.email = settings.get_my_studio().email.into();
     config.my_studio.company_id = settings.get_my_studio().company_id.into();
